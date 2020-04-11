ba7092b79b01a69e248a5477ad1ff260eae2b303
Mithlesh Yadav <32195144+mithleshfreak@users.noreply.github.com>
2020-04-11 18:06:06 +0545

    Update Contants.js

--- src/constants/Contants.js
+++ src/constants/Contants.js
@@ -9,8 +9,4 @@ export const Android_Status_Bar_height = ExtraDimensions.getStatusBarHeight()
 export const SCREEN_HEIGHT = () => IS_IOS ? Dimensions.get('window').height : ExtraDimensions.getRealWindowHeight() - ExtraDimensions.getSoftMenuBarHeight() - Android_Status_Bar_height()
 export const SCREEN_WIDTH = () => Dimensions.get('window').width
 
-
-export const AUTH_KEY = 'githubcommits:auth'
-export const USERDATA_KEY = 'githubcommits:user'
-
 export const BASE_URL = 'https://api.github.com'
