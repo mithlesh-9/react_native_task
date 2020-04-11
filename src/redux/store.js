6506a092bb17c0d4d60f70fde2eb9df4a7372f21
Mithlesh Yadav <32195144+mithleshfreak@users.noreply.github.com>
2020-04-11 18:02:11 +0545

    these files has been modified to configure store and reducer.

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import reducers from './root-reducer'
 
+
 let middlewares = [thunk]
 
 export const store = createStore(reducers,applyMiddleware(...middlewares))
