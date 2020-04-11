fd825a5e39fe5883d6eaf3198a2735eb99c5b7ee
Mithlesh Yadav <32195144+mithleshfreak@users.noreply.github.com>
2020-04-11 18:23:30 +0545

    New Screens has been added.

--- src/screens/Loading.js
+++ src/screens/Loading.js
@@ -6,70 +6,34 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  StyleSheet,
   View,
   ActivityIndicator
 } from 'react-native'
-
-import { createStackNavigator } from '@react-navigation/stack'
-
-import Login from './Login'
-import Password from './Password'
+import { connect } from 'react-redux'
+import { getAuthData } from '../redux/user/user.actions'
+import Main from './Main'
 
 
 
-const Stack = createStackNavigator()
 
-const AuthScreens = () => (
-  <>
-      <Stack.Screen 
-        name="Login"
-        component={Login}
-        options={{
-          headerShown:false
-        }}  
-
-      />
-      <Stack.Screen 
-        name="Password"
-        component={Password}
-        
-      />
-  </>
-)
-
-const MainScreens = () => (
-  <Stack.Screen 
-        name="Home"
-        component={Password} 
-  />
-)
-
-const isAuthenticated = false
-
-function MainStack() {
-  return (
-    <Stack.Navigator
-    >
-      { isAuthenticated 
-        ? MainScreens()
-        : AuthScreens()
-      }
-    </Stack.Navigator>
+const Loading = (props) =>  {
+  const [ready, setReady] = useState(false)
+  useEffect(()=> {
+    props.dispatch(getAuthData())
+         .then(()=>setReady(true))
+  },[])
+    return (
+        <View style={{flex:1}}>
+            {ready 
+              ? <Main/>
+              : <View style={{flex:1,justifyContent:'center',alignItems:'center'}}><ActivityIndicator size={40} color="#111" /></View>
+            }
+        </View>
   )
 }
 
 
-const Loading = () => {
-  return (
-        <View>
-            <ActivityIndicator/>
-        </View>
-  );
-};
 
-
-
-export default Loading
+export default connect()(Loading)
